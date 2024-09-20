0389aeaa80ace6a3ff12d94ad5d520db8d8f00a1
Jesus Angarita <jes.angar11@gmail.com>
2024-09-20 11:17:46 -0500

    Jesus Angarita modulo de usuarios dia 20/09 a las 11:17am

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
